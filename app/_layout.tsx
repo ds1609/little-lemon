dd809555b76d4039788806e45c5e2a0b850a46db
ds1609 <dsarigam1709@gmail.com>
2025-04-19 15:34:43 +0530

    updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,98 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { Stack, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Redirect, usePathname } from 'expo-router';
+import { View, ActivityIndicator, Image, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+  const [isReady, setIsReady] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const [shortName, setShortName] = useState("");
+
+  const checkLoginStatus = async () => {
+    const userData = await AsyncStorage.getItem('user');
+    if (userData) {
+      const user = JSON.parse(userData);
+      let fName = user.first_name ? user.first_name.charAt(0) : "";
+      let lName = user.last_name ? user.last_name.charAt(0) : "";
+      let shortName = fName+lName;
+      setShortName(shortName);
+      
+      setTimeout(() => {
+        router.replace('/profile');
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        router.replace('/'); // or login screen
+      }, 2000);
+    }
+  };
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+    checkLoginStatus();
+  }, []);
 
-  if (!loaded) {
-    return null;
-  }
-
+  
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown:false }} />
+      <Stack.Screen name="home" />
+      <Stack.Screen name="profile" options={{
+        headerTitle: () => { 
+          return (
+            <Image 
+              source={require("../assets/images/Logo.png")} 
+              style={{
+                height:56,
+                width:179,
+                resizeMode:"contain",
+                alignSelf:"center"
+              }} />
+          ); 
+        },
+        headerRight: () => {
+          return (
+            // <Image 
+            //   source={require("../assets/images/Profile.png")} 
+            //   style={{
+            //     height:47,
+            //     width:50,
+            //     resizeMode:"contain"
+            //   }} />
+            <View style={{
+              backgroundColor:"#495E57",
+              height:44,
+              width:44,
+              borderRadius:22,
+              justifyContent:"center",
+              // marginTop:10
+            }}>
+              <Text style={{
+                textAlign:"center",
+                fontFamily:"Karla-Regular",
+                fontSize:22,
+                color:"#EDEFEE"
+              }}>{shortName}</Text>
+            </View>
+          );
+        },
+        headerLeft: () => {
+          return (
+            <Ionicons
+              name="arrow-back"
+              size={24}
+              color="white"
+              style={{ backgroundColor:"#495E57", padding:5, borderRadius:50,opacity:0.8 }}
+              // onPress={() => router.back()}
+              onPress={() => {}}
+            />
+          );
+        }
+      }} />
+    </Stack>
   );
 }
