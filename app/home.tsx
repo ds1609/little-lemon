bb3e5c70d2ba03800e1d5c1bc0f7ad35dc6ad231
ds1609 <dsarigam1709@gmail.com>
2025-04-25 17:25:56 +0530

    initial commit

--- app/home.tsx
+++ app/home.tsx
@@ -1,13 +1,152 @@
-import { useRouter } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Text, View, Image, TextInput, ScrollView } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { FlashList } from "@shopify/flash-list";
+import { createTable, getMenuItems, saveMenuItems, filterByQueryAndCategories } from "../database";
+import Filters from "./components/filters";
+import { getSectionListData, useUpdateEffect }  from "../utils";
+import debounce from "lodash/debounce";
+import { homeStyles } from "../styles/home.styles";
+
+const API_URL = 'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json';
 
 export default function Home() {
-  const router = useRouter();
+  const [searchText, setSearchText] = useState("");
+  const [query, setQuery] = useState("");
+  const [menus, setMenus] = useState(Object);
+  const [sections, setSections] = useState(Object);
+  const [filterSelections, setFilterSelections] = useState<boolean[]>([]);
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        // create table
+        await createTable();
+
+        // get menu items
+        let menuItems = await getMenuItems();
+        if(menuItems?.length == 0){
+          const menuItems = await fetchData();
+          saveMenuItems(menuItems);
+        }
+
+        setMenus(menuItems);
+
+        // Get Sections Categories
+        const sectionListData = getSectionListData(menuItems);
+        setSections(sectionListData);
+
+        setFilterSelections(sectionListData.map(() => false));
+
+      } catch (error) {
+        console.log("useEffect Error",error);
+      }
+    })();
+  }, []);
+
+  const fetchData = async() => {
+    try {
+      const res = await fetch(API_URL);
+      const json = await res.json();
+      const menuData = json.menu;
+      return menuData;
+    } catch (error) {
+      console.log("fetchData Error",error);
+    }
+  }
+
+  useUpdateEffect(() => {
+    (async () => {
+      const activeCategory = sections.filter((s,i) => {
+        if (filterSelections.every((item) => item === false)) {
+          return true;
+        }
+        return filterSelections[i];
+      })
+      
+      try {
+        const menuItems = await filterByQueryAndCategories(query,activeCategory);
+        setMenus(menuItems);
+      } catch (error) {
+        console.log("",error);
+      }
+      
+    })();
+  },[filterSelections,query]);
+
+  const handleFiltersChange = async (index:number) => {
+    let arrayCopy = [...filterSelections];
+    arrayCopy[index] = !filterSelections[index];
+    setFilterSelections(arrayCopy);
+  }
+
+  const lookup = useCallback((q:any) => {
+    setQuery(q);
+  }, []);
+  const debouncedLookUp = useMemo(() => debounce(lookup, 500),[lookup]);
+
+  const handleSearchBar = (text:string) => {
+    setSearchText(text);
+    debouncedLookUp(text);
+  }
+
+  const renderItem = ({ item }) => {
+    let photoUrl = `https://github.com/ds1609/little-lemon/blob/development/assets/images/${item.image}?raw=true`;
+    
+    return (
+      <View style={homeStyles.sectionMenus}>
+        <Text style={homeStyles.menuTitle}>{item.name}</Text>
+        <View style={{flexDirection:"row"}}>
+          <View style={{width:"70%"}}>
+            <Text style={homeStyles.menuDescription}>{item.description}</Text>
+            <Text style={homeStyles.menuPrice}>{"$"}{item.price}</Text>
+          </View>
+          <View style={homeStyles.sectionMenuImage}>
+            <Image source={{uri:photoUrl}} style={homeStyles.menuImage} />
+          </View>
+        </View>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
-      <Button title="Go to Profile" onPress={() => router.navigate("/profile")}/>
-    </View>
+    <ScrollView style={homeStyles.container}>
+      <View style={homeStyles.sectionBanner}>
+        <Text style={homeStyles.bannerTitle}>Little lemon</Text>
+        
+        <View style={{flexDirection:"row"}}>
+          <View style={{width:"65%"}}>
+            <Text style={homeStyles.bannerSubTitle}>Chicago</Text>
+            <Text style={homeStyles.bannerDescription}>We are a family owned Mediterranean restaurant, focused on traditional recipes served wth a modern twist.</Text>
+          </View>
+          <View style={homeStyles.sectionBannerImage}>
+            <Image source={require("../assets/images/hero_image.png")} style={homeStyles.bannerImage} />
+          </View>
+        </View>
+
+        <View style={homeStyles.sectionBannerSearch}>
+          <Ionicons name="search" style={homeStyles.iconSearch} />
+          <TextInput style={[homeStyles.styleInput,{flex:1}]} placeholder="search here..." placeholderTextColor={"#333333"} value={searchText} onChangeText={handleSearchBar} />
+        </View>
+        
+      </View>
+
+      <View style={homeStyles.sectionFilters}>
+        <Text style={homeStyles.sectionFiltersTitle}>Order for Delivery</Text>
+        
+        {Object.keys(sections).length > 0 ? (
+          <Filters sections={sections} onChange={handleFiltersChange} selections={filterSelections} />
+        ) : null }
+      </View>
+
+      <FlashList
+        data={menus}
+        renderItem={renderItem}
+        ListEmptyComponent={<Text style={{fontFamily:"Karla-Regular",fontSize:20,fontWeight:"bold",margin:25}}>No Data</Text>}
+        estimatedItemSize={200}
+      />
+
+    </ScrollView>
   );
 }
 
---
+++ styles/home.styles.js
@@ -0,0 +1,99 @@
+import { StyleSheet } from "react-native";
+
+export const homeStyles = StyleSheet.create({
+    container:{
+      flex: 1,
+      backgroundColor:"white"
+    },
+    styleInput: {
+      fontFamily:"Karla-Regular",
+      fontSize:18,
+    },
+    sectionBanner: {
+      backgroundColor:"#495E57",
+      paddingHorizontal:25,
+      paddingVertical:20
+    },
+    bannerTitle: {
+      color:"#F4CE14",
+      fontFamily:"MarkaziText-Regular",
+      fontSize:64,
+      fontWeight:"medium"
+    },
+    bannerSubTitle: {
+      color:"#EDEFEE",
+      fontFamily:"MarkaziText-Regular",
+      fontSize:40,
+      fontWeight:"regular"
+    },
+    bannerDescription: {
+      color:"#EDEFEE",
+      fontFamily:"Karla-Regular",
+      fontSize:18,
+      fontWeight:"medium"
+    },
+    sectionBannerImage: {
+      width:"35%",
+      alignItems:"flex-end"
+    },
+    bannerImage: {
+      width:125, 
+      height:171, 
+      borderRadius:16,
+      resizeMode:"cover"
+    },
+    sectionBannerSearch: {
+      flexDirection:"row",
+      borderRadius:8,
+      backgroundColor:"#EDEFEE",
+      paddingHorizontal:10,
+      marginTop:15
+    },
+    iconSearch: {
+      marginRight:8,
+      alignSelf:"center",
+      color:"#333333",
+      fontSize:20
+    },
+    sectionFilters: {
+      padding:25,
+      borderBottomWidth:1,
+      borderBottomColor:"#EDEFEE"
+    },
+    sectionFiltersTitle: {
+      fontFamily:"Karla-Regular",
+      fontSize:24,
+      fontWeight:"bold",
+      marginBottom:43
+    },
+    sectionMenus: {
+      backgroundColor:"#FFFFFF",
+      padding:25
+    },
+    menuTitle: {
+      fontFamily:"Karla-Regular",
+      fontSize:20,
+      fontWeight:"bold",
+      marginBottom:10
+    },
+    menuDescription: {
+      fontFamily:"Karla-Regular",
+      fontSize:16,
+      fontWeight:"regular",
+      marginBottom:10
+    },
+    menuPrice: {
+      fontFamily:"Karla-Regular",
+      fontSize:20,
+      fontWeight:"bold"
+    },
+    sectionMenuImage: {
+      width:"30%",
+      alignItems:"flex-end"
+    },
+    menuImage: {
+      width:80, 
+      height:80, 
+      resizeMode:"cover"
+    }
+  });
