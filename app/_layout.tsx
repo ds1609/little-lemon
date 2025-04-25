5828d5fe787a031b4bc544aa492a964f51c550f0
ds1609 <dsarigam1709@gmail.com>
2025-04-25 16:53:46 +0530

    header component updated with back button, logo and profile picture

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,11 @@
 import { Stack, useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Redirect, usePathname } from 'expo-router';
-import { View, ActivityIndicator, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function RootLayout() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
-  const [isReady, setIsReady] = useState(false);
-  const pathname = usePathname();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
 
   const [shortName, setShortName] = useState("");
@@ -21,9 +18,9 @@ export default function RootLayout() {
       let lName = user.last_name ? user.last_name.charAt(0) : "";
       let shortName = fName+lName;
       setShortName(shortName);
-      
+      setIsLoggedIn(true);
       setTimeout(() => {
-        router.replace('/profile');
+        router.replace('/home');
       }, 2000);
     } else {
       setTimeout(() => {
@@ -36,11 +33,52 @@ export default function RootLayout() {
     checkLoginStatus();
   }, []);
 
+  const handleProfileIcon = () => {
+    router.navigate("/profile");
+  }
   
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown:false }} />
-      <Stack.Screen name="home" />
+      <Stack.Screen name="home" options={{
+        headerTitle: () => { 
+          return (
+            <Image 
+              source={require("../assets/images/Logo.png")} 
+              style={{
+                height:56,
+                width:179,
+                resizeMode:"contain",
+                alignSelf:"center"
+              }} />
+          ); 
+        },
+        headerRight: () => {
+          return (
+            // <Image 
+            //   source={require("../assets/images/Profile.png")} 
+            //   style={{
+            //     height:47,
+            //     width:50,
+            //     resizeMode:"contain"
+            //   }} />
+            <TouchableOpacity style={{
+              backgroundColor:"#495E57",
+              height:44,
+              width:44,
+              borderRadius:22,
+              justifyContent:"center"
+            }} onPress={handleProfileIcon}>
+              <Text style={{
+                textAlign:"center",
+                fontFamily:"Karla-Regular",
+                fontSize:22,
+                color:"#EDEFEE"
+              }}>{shortName}</Text>
+            </TouchableOpacity>
+          );
+        },
+      }} />
       <Stack.Screen name="profile" options={{
         headerTitle: () => { 
           return (
@@ -68,8 +106,7 @@ export default function RootLayout() {
               height:44,
               width:44,
               borderRadius:22,
-              justifyContent:"center",
-              // marginTop:10
+              justifyContent:"center"
             }}>
               <Text style={{
                 textAlign:"center",
@@ -82,14 +119,18 @@ export default function RootLayout() {
         },
         headerLeft: () => {
           return (
-            <Ionicons
-              name="arrow-back"
-              size={24}
-              color="white"
-              style={{ backgroundColor:"#495E57", padding:5, borderRadius:50,opacity:0.8 }}
-              // onPress={() => router.back()}
-              onPress={() => {}}
-            />
+            <TouchableOpacity onPress={() => { 
+              if(isLoggedIn){
+                router.back();
+              }
+             }}>
+              <Ionicons
+                name="arrow-back"
+                size={24}
+                color="white"
+                style={{ backgroundColor:"#495E57", padding:5, borderRadius:50,opacity: isLoggedIn ? 1 : 0.8 }}
+              />
+            </TouchableOpacity>
           );
         }
       }} />
