bda201fc013d54bb9ef5e025ea5ce9979955f456
ds1609 <dsarigam1709@gmail.com>
2025-04-27 18:07:15 +0530

    separate files created for header elements

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack, useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Ionicons } from '@expo/vector-icons';
+
+import HeaderTitle from "./components/headerTitle";
+import HeaderRight from "./components/headerRight";
+import HeaderLeft from "./components/headerLeft";
 
 export default function RootLayout() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
 
+  const [image, setImage] = useState<string | null>(null);
   const [shortName, setShortName] = useState("");
 
   const checkLoginStatus = async () => {
@@ -18,6 +22,8 @@ export default function RootLayout() {
       let lName = user.last_name ? user.last_name.charAt(0) : "";
       let shortName = fName+lName;
       setShortName(shortName);
+      setImage(user.photo);
+
       setIsLoggedIn(true);
       setTimeout(() => {
         router.replace('/home');
@@ -34,105 +40,28 @@ export default function RootLayout() {
   }, []);
 
   const handleProfileIcon = () => {
+    // console.log("handleProfileIcon");
     router.navigate("/profile");
+  }
+
+  const handleBackButton = () => {
+    // console.log("handleBackButton");
+    if(isLoggedIn){
+      router.back();
+    }
   }
   
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown:false }} />
       <Stack.Screen name="home" options={{
-        headerTitle: () => { 
-          return (
-            <Image 
-              source={require("../assets/images/Logo.png")} 
-              style={{
-                height:56,
-                width:179,
-                resizeMode:"contain",
-                alignSelf:"center"
-              }} />
-          ); 
-        },
-        headerRight: () => {
-          return (
-            // <Image 
-            //   source={require("../assets/images/Profile.png")} 
-            //   style={{
-            //     height:47,
-            //     width:50,
-            //     resizeMode:"contain"
-            //   }} />
-            <TouchableOpacity style={{
-              backgroundColor:"#495E57",
-              height:44,
-              width:44,
-              borderRadius:22,
-              justifyContent:"center"
-            }} onPress={handleProfileIcon}>
-              <Text style={{
-                textAlign:"center",
-                fontFamily:"Karla-Regular",
-                fontSize:22,
-                color:"#EDEFEE"
-              }}>{shortName}</Text>
-            </TouchableOpacity>
-          );
-        },
+        headerTitle: () => <HeaderTitle  />,
+        headerRight: () => <HeaderRight image={image} shortName={shortName} handleProfileIcon={handleProfileIcon} />,
       }} />
       <Stack.Screen name="profile" options={{
-        headerTitle: () => { 
-          return (
-            <Image 
-              source={require("../assets/images/Logo.png")} 
-              style={{
-                height:56,
-                width:179,
-                resizeMode:"contain",
-                alignSelf:"center"
-              }} />
-          ); 
-        },
-        headerRight: () => {
-          return (
-            // <Image 
-            //   source={require("../assets/images/Profile.png")} 
-            //   style={{
-            //     height:47,
-            //     width:50,
-            //     resizeMode:"contain"
-            //   }} />
-            <View style={{
-              backgroundColor:"#495E57",
-              height:44,
-              width:44,
-              borderRadius:22,
-              justifyContent:"center"
-            }}>
-              <Text style={{
-                textAlign:"center",
-                fontFamily:"Karla-Regular",
-                fontSize:22,
-                color:"#EDEFEE"
-              }}>{shortName}</Text>
-            </View>
-          );
-        },
-        headerLeft: () => {
-          return (
-            <TouchableOpacity onPress={() => { 
-              if(isLoggedIn){
-                router.back();
-              }
-             }}>
-              <Ionicons
-                name="arrow-back"
-                size={24}
-                color="white"
-                style={{ backgroundColor:"#495E57", padding:5, borderRadius:50,opacity: isLoggedIn ? 1 : 0.8 }}
-              />
-            </TouchableOpacity>
-          );
-        }
+        headerTitle: () => <HeaderTitle />,
+        headerRight: () => <HeaderRight image={image} shortName={shortName} handleProfileIcon={handleProfileIcon} />,
+        headerLeft: () => <HeaderLeft isLoggedIn={isLoggedIn} handleBackButton={handleBackButton} />
       }} />
     </Stack>
   );
